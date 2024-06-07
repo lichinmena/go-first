4c4bb27c4baaa62b1e75b7c1e6ea9385864c6dd6
Luis <luis_mena@outlook.com>
2024-06-06 23:02:32 -0600

    Structs

---
+++ estructura_structs/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// Creacion
+type Persona struct {
+	nombre string
+	edad   int
+	correo string
+}
+
+func main() {
+	//Instanciar
+	var p Persona
+	p.nombre = "Luis"
+	p.edad = 35
+	p.correo = "test@outlook.com"
+	fmt.Println(p)
+
+	//Otra forma de instanciar
+	p2 := Persona{"Fernando", 65, "asas@outlook.com"}
+	fmt.Println(p2)
+
+	fmt.Println(p.nombre)
+	fmt.Println(p.edad)
+	fmt.Println(p.correo)
+}
