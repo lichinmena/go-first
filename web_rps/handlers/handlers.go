ec2a589e5d1a53deb8f99c539c96cdd4e8036176
Luis <luis_mena@outlook.com>
2024-06-27 00:06:30 -0600

    Platillas, pasar datos del handler al html, composicion

--- web_rps/handlers/handlers.go
+++ web_rps/handlers/handlers.go
@@ -2,11 +2,31 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Pagina de inicio")
+	tpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
+	if err != nil {
+		http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
+		return
+	}
+
+	/*
+		data := struct {
+			Title   string
+			Message string
+		}{
+			Title:   "Pagina de inicio",
+			Message: "Bienvenido a piedra, papel y tijera",
+		}
+	*/
+	err = tpl.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+		return
+	}
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
